0365a7e73a1e3e12c96ec5f4eab2dea46c40d6d0
arvinpaundra <arvinpaundra06@gmail.com>
2023-11-23 08:24:10 +0700

    feat(auth): auth database migration

--- internal/factory/factory.go
+++ internal/factory/factory.go
@@ -1,8 +1,41 @@
 package factory
 
+import (
+	"context"
+
+	"github.com/arvinpaundra/ngekost-api/internal/driver/contract"
+	"github.com/arvinpaundra/ngekost-api/internal/driver/postgres"
+	txBeginner "github.com/arvinpaundra/ngekost-api/internal/driver/postgres/beginner"
+	"github.com/arvinpaundra/ngekost-api/internal/driver/postgres/lessee"
+	"github.com/arvinpaundra/ngekost-api/internal/driver/postgres/owner"
+	"github.com/arvinpaundra/ngekost-api/internal/driver/postgres/session"
+	"github.com/arvinpaundra/ngekost-api/internal/driver/postgres/user"
+	"github.com/arvinpaundra/ngekost-api/internal/driver/redis"
+	"github.com/arvinpaundra/ngekost-api/internal/driver/redis/cache"
+	"github.com/arvinpaundra/ngekost-api/pkg/util/token"
+)
+
 type Factory struct {
+	CacheRepository   contract.CacheRepository
+	TxBeginner        contract.TxBeginner
+	UserRepository    contract.UserRepository
+	OwnerRepository   contract.OwnerRepository
+	LesseeRepository  contract.LesseeRepository
+	SessionRepository contract.SessionRepository
+	JSONWebToken      token.JSONWebToken
 }
 
-func NewFactory() *Factory {
-	return &Factory{}
+func NewFactory(ctx context.Context) *Factory {
+	rdb := redis.New().Connect(ctx)
+	pg := postgres.New().Connect(ctx)
+
+	return &Factory{
+		JSONWebToken:      token.NewJWT(),
+		CacheRepository:   cache.NewCacheRepository(rdb),
+		TxBeginner:        txBeginner.NewTxBeginner(pg),
+		UserRepository:    user.NewAuthRepository(pg),
+		OwnerRepository:   owner.NewOwnerRepository(pg),
+		LesseeRepository:  lessee.NewLesseeRepository(pg),
+		SessionRepository: session.NewSessionRepository(pg),
+	}
 }
--- internal/http/http.go
+++ internal/http/http.go
@@ -1,13 +1,16 @@
 package http
 
 import (
+	"github.com/arvinpaundra/ngekost-api/internal/app/auth"
 	"github.com/arvinpaundra/ngekost-api/internal/app/ping"
 	"github.com/arvinpaundra/ngekost-api/internal/factory"
 	"github.com/gofiber/fiber/v2"
 )
 
 func NewHttp(app *fiber.App, f *factory.Factory) {
-	_ = app.Group("/api/v1")
+	v1 := app.Group("/api/v1")
+
+	auth.NewClient(f).RouterV1(v1.Group("/auth"))
 
 	ping.NewClient(f).Router(app.Group(""))
 }
