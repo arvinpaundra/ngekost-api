2484a21a0f46bb2265b8cedb2dcc33a38db5d54e
arvinpaundra <arvinpaundra06@gmail.com>
2023-11-26 17:27:35 +0700

    feat(kost): kost error conventions

--- pkg/constant/error.go
+++ pkg/constant/error.go
@@ -3,9 +3,13 @@ package constant
 import "errors"
 
 var (
-	ErrUserNotFound    = errors.New("user not found")
-	ErrOwnerNotFound   = errors.New("owner not found")
-	ErrSessionNotFound = errors.New("session not found")
+	ErrUserNotFound      = errors.New("user not found")
+	ErrOwnerNotFound     = errors.New("owner not found")
+	ErrSessionNotFound   = errors.New("session not found")
+	ErrKostNotFound      = errors.New("kost not found")
+	ErrRoomNotFound      = errors.New("room not found")
+	ErrRoomAssetNotFound = errors.New("room asset not found")
+	ErrKostRuleNotFound  = errors.New("kost rule not found")
 
 	ErrUsernameAlreadyUsed = errors.New("username already used")
 	ErrPasswordIncorrect   = errors.New("password is incorrect")
