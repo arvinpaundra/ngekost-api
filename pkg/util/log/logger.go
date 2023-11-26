8b6b5fbbcc5373071c8b73173b3e3f5d4309b76c
arvinpaundra <arvinpaundra06@gmail.com>
2023-11-26 17:18:14 +0700

    fix: bugfix display error func and file location

--- pkg/util/log/logger.go
+++ pkg/util/log/logger.go
@@ -2,32 +2,13 @@ package log
 
 import "github.com/sirupsen/logrus"
 
-type logger struct {
-	log     *logrus.Logger
-	message string
-}
-
-func Logging(msg string) *logger {
-	l := logger{}
-	l.log = logrus.New()
-	l.log.SetFormatter(&logrus.JSONFormatter{
+func Logging(msg ...string) *logrus.Logger {
+	l := logrus.New()
+	l.SetFormatter(&logrus.JSONFormatter{
 		TimestampFormat: "2006-02-02 15:04:05",
 		PrettyPrint:     true,
 	})
-	l.log.SetReportCaller(true)
-	l.message = msg
+	l.SetReportCaller(true)
 
-	return &l
-}
-
-func (l *logger) Info() {
-	l.log.Info(l.message)
-}
-
-func (l *logger) Error() {
-	l.log.Error(l.message)
-}
-
-func (l *logger) Warn() {
-	l.log.Warn(l.message)
+	return l
 }
