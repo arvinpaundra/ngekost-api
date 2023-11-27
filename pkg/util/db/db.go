00932f40b2ec208e817f8f6e9eeeee3340aed792
arvinpaundra <arvinpaundra06@gmail.com>
2023-11-27 12:15:54 +0700

    chore: gorm scope for exact match condition

--- pkg/util/db/db.go
+++ pkg/util/db/db.go
@@ -1,13 +1,29 @@
 package db
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"strings"
+
+	"gorm.io/gorm"
+)
 
 type gormScope func(*gorm.DB) *gorm.DB
 
 func Search(col, search string) gormScope {
 	return func(db *gorm.DB) *gorm.DB {
 		if search != "" {
-			return db.Where(col+" LIKE ?", "%"+search+"%")
+			lowerCol := fmt.Sprintf("LOWER(%s)", col)
+			lowerSearch := strings.ToLower(search)
+			return db.Where(lowerCol+" LIKE ?", "%"+lowerSearch+"%")
+		}
+		return db
+	}
+}
+
+func Exact(col, val string) gormScope {
+	return func(db *gorm.DB) *gorm.DB {
+		if val != "" {
+			return db.Where(col+" = ?", val)
 		}
 		return db
 	}
