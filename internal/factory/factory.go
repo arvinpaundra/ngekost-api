1cf17cff19e303cbe2be8595c77ba7e1cac6df3f
arvinpaundra <arvinpaundra06@gmail.com>
2023-11-27 12:08:45 +0700

    feat(payment): payment factory

--- internal/factory/factory.go
+++ internal/factory/factory.go
@@ -4,12 +4,16 @@ import (
 	"context"
 
 	"github.com/arvinpaundra/ngekost-api/internal/driver/contract"
+	"github.com/arvinpaundra/ngekost-api/internal/driver/midtrans"
+	"github.com/arvinpaundra/ngekost-api/internal/driver/midtrans/transaction"
 	"github.com/arvinpaundra/ngekost-api/internal/driver/postgres"
 	txBeginner "github.com/arvinpaundra/ngekost-api/internal/driver/postgres/beginner"
+	"github.com/arvinpaundra/ngekost-api/internal/driver/postgres/bill"
 	"github.com/arvinpaundra/ngekost-api/internal/driver/postgres/kost"
 	kostrule "github.com/arvinpaundra/ngekost-api/internal/driver/postgres/kostRule"
 	"github.com/arvinpaundra/ngekost-api/internal/driver/postgres/lessee"
 	"github.com/arvinpaundra/ngekost-api/internal/driver/postgres/owner"
+	"github.com/arvinpaundra/ngekost-api/internal/driver/postgres/payment"
 	"github.com/arvinpaundra/ngekost-api/internal/driver/postgres/room"
 	roomasset "github.com/arvinpaundra/ngekost-api/internal/driver/postgres/roomAsset"
 	"github.com/arvinpaundra/ngekost-api/internal/driver/postgres/session"
@@ -20,34 +24,41 @@ import (
 )
 
 type Factory struct {
-	JSONWebToken        token.JSONWebToken
-	CacheRepository     contract.CacheRepository
-	TxBeginner          contract.TxBeginner
-	UserRepository      contract.UserRepository
-	OwnerRepository     contract.OwnerRepository
-	LesseeRepository    contract.LesseeRepository
-	SessionRepository   contract.SessionRepository
-	KostRepository      contract.KostRepository
-	RoomRepository      contract.RoomRepository
-	KostRuleRepository  contract.KostRuleRepository
-	RoomAssetRepository contract.RoomAssetRepository
+	JSONWebToken                  token.JSONWebToken
+	CacheRepository               contract.CacheRepository
+	MidtransTransactionRepository contract.MidtransTransactionRepository
+	TxBeginner                    contract.TxBeginner
+	UserRepository                contract.UserRepository
+	OwnerRepository               contract.OwnerRepository
+	LesseeRepository              contract.LesseeRepository
+	SessionRepository             contract.SessionRepository
+	KostRepository                contract.KostRepository
+	RoomRepository                contract.RoomRepository
+	KostRuleRepository            contract.KostRuleRepository
+	RoomAssetRepository           contract.RoomAssetRepository
+	BillRepository                contract.BillRepository
+	PaymentRepository             contract.PaymentRepository
 }
 
 func NewFactory(ctx context.Context) *Factory {
 	rdb := redis.New().Connect(ctx)
 	pg := postgres.New().Connect(ctx)
+	sc := midtrans.New().Snap()
 
 	return &Factory{
-		JSONWebToken:        token.NewJWT(),
-		CacheRepository:     cache.NewCacheRepository(rdb),
-		TxBeginner:          txBeginner.NewTxBeginner(pg),
-		UserRepository:      user.NewAuthRepository(pg),
-		OwnerRepository:     owner.NewOwnerRepository(pg),
-		LesseeRepository:    lessee.NewLesseeRepository(pg),
-		SessionRepository:   session.NewSessionRepository(pg),
-		KostRepository:      kost.NewKostRepository(pg),
-		RoomRepository:      room.NewRoomRepository(pg),
-		KostRuleRepository:  kostrule.NewKostRuleRepository(pg),
-		RoomAssetRepository: roomasset.NewRoomAssetRepository(pg),
+		JSONWebToken:                  token.NewJWT(),
+		CacheRepository:               cache.NewCacheRepository(rdb),
+		MidtransTransactionRepository: transaction.NewTransactionRepository(sc),
+		TxBeginner:                    txBeginner.NewTxBeginner(pg),
+		UserRepository:                user.NewAuthRepository(pg),
+		OwnerRepository:               owner.NewOwnerRepository(pg),
+		LesseeRepository:              lessee.NewLesseeRepository(pg),
+		SessionRepository:             session.NewSessionRepository(pg),
+		KostRepository:                kost.NewKostRepository(pg),
+		RoomRepository:                room.NewRoomRepository(pg),
+		KostRuleRepository:            kostrule.NewKostRuleRepository(pg),
+		RoomAssetRepository:           roomasset.NewRoomAssetRepository(pg),
+		BillRepository:                bill.NewBillRepository(pg),
+		PaymentRepository:             payment.NewPaymentRepository(pg),
 	}
 }
