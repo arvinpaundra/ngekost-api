f8acbc5b09c4f0821ff2cee94873afeadd0ec335
arvinpaundra <arvinpaundra06@gmail.com>
2023-11-28 10:58:18 +0700

    fic: bugfix create bills table

--- database/migrations/000009_create_table_bills.up.sql
+++ database/migrations/000009_create_table_bills.up.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS bills (
   id CHAR(32) PRIMARY KEY,
   lessee_id CHAR(32) NOT NULL,
   invoice VARCHAR(20) NOT NULL,
-  kost_name VARCHAR(250) NOT NULL
-  room_name VARCHAR(250) NOT NULL
+  kost_name VARCHAR(250) NOT NULL,
+  room_name VARCHAR(250) NOT NULL,
   rent_type VARCHAR(8) NOT NULL,
   status VARCHAR(8) NOT NULL,
   price NUMERIC(11, 2) NOT NULL,
