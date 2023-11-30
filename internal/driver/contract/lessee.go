1497d36c2d6b868576e9e664b3de094d1bdc143d
arvinpaundra <arvinpaundra06@gmail.com>
2023-11-30 21:54:57 +0700

    refactor: refactoring handle database transaction inside service layer

--- internal/driver/contract/lessee.go
+++ internal/driver/contract/lessee.go
@@ -4,14 +4,11 @@ import (
 	"context"
 
 	"github.com/arvinpaundra/ngekost-api/internal/entity"
-	"gorm.io/gorm"
 )
 
 type LesseeRepository interface {
 	Save(ctx context.Context, lessee *entity.Lessee) error
 	Update(ctx context.Context, lessee *entity.Lessee, ownerId string) error
-	SaveWithTx(ctx context.Context, tx *gorm.DB, lessee *entity.Lessee) error
-	UpdateWithTx(ctx context.Context, tx *gorm.DB, lessee *entity.Lessee, ownerId string) error
 	Find(ctx context.Context, keyword string) ([]*entity.Lessee, error)
 	FindById(ctx context.Context, id string) (*entity.Lessee, error)
 	Count(ctx context.Context, keyword string) (int, error)
--- internal/driver/contract/user.go
+++ internal/driver/contract/user.go
@@ -9,7 +9,6 @@ import (
 
 type UserRepository interface {
 	Save(ctx context.Context, user *entity.User) error
-	SaveWithTx(ctx context.Context, tx *gorm.DB, user *entity.User) error
 	FindByUsername(ctx context.Context, username string) (*entity.User, error)
 	FindById(ctx context.Context, userId string) (*entity.User, error)
 	Update(ctx context.Context, tx *gorm.DB, user *entity.User, userId string) error
