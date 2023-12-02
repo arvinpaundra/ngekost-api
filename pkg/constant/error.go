032d884f3c5e48490795cc4fb137d907f3789fc4
arvinpaundra <arvinpaundra06@gmail.com>
2023-12-02 13:07:18 +0700

    feat(rent): rent errors conventions

--- pkg/constant/error.go
+++ pkg/constant/error.go
@@ -12,8 +12,11 @@ var (
 	ErrKostRuleNotFound  = errors.New("kost rule not found")
 	ErrBillNotFound      = errors.New("bill not found")
 	ErrPaymentNotFound   = errors.New("payment not found")
+	ErrRentNotFound      = errors.New("rent not found")
+	ErrLesseeNotFound    = errors.New("lessee not found")
 
 	ErrUsernameAlreadyUsed = errors.New("username already used")
 	ErrPasswordIncorrect   = errors.New("password is incorrect")
 	ErrInvalidRole         = errors.New("invalid role")
+	ErrActiveRent          = errors.New("you have an active rent")
 )
